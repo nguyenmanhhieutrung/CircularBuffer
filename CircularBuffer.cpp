80e3c1d6ca2bf4e2dbb875f85d575a8e83905b4f
nguyenmanhhieutrung <nguyenmanhhieutrung@gmail.com>
2016-09-12 07:28:06 +0700

    Update CircularBuffer.cpp

--- CircularBuffer.cpp
+++ CircularBuffer.cpp
@@ -15,12 +15,12 @@ CircularBuffer::~CircularBuffer() {
 
 size_t CircularBuffer::Write(BYTE* pInputData, size_t pDataSize) {
 	size_t write_size = std::min(pDataSize, m_Free);
-	size_t first_half = std::min(write_size, (m_Capacity-m_Head) );
-	size_t second_half = write_size - first_half;
+	size_t first_piece = std::min(write_size, (m_Capacity-m_Head) );
+	size_t second_piece = write_size - first_piece;
 
-	memcpy( (m_Data+m_Head), pInputData, first_half);
-	if ( second_half > 0 )
-		memcpy( m_Data, (pInputData+first_half), second_half);
+	memcpy( (m_Data+m_Head), pInputData, first_piece);
+	if ( second_piece > 0 )
+		memcpy( m_Data, (pInputData+first_piece), second_piece);
 
 	Step(m_Head,write_size);
 	m_Free = m_Free-write_size;
@@ -30,12 +30,12 @@ size_t CircularBuffer::Write(BYTE* pInputData, size_t pDataSize) {
 
 size_t CircularBuffer::Read(BYTE* pOutData, size_t pDataSize) {
 	size_t read_size = std::min(pDataSize, m_Capacity-m_Free);
-	size_t first_half = std::min(read_size, (m_Capacity-m_Tail) );
-	size_t second_half = read_size - first_half;
+	size_t first_piece = std::min(read_size, (m_Capacity-m_Tail) );
+	size_t second_piece = read_size - first_piece;
 
-	memcpy(pOutData, (m_Data+m_Tail), first_half);
-	if ( second_half > 0 )
-		memcpy(pOutData+first_half, m_Data, second_half);
+	memcpy(pOutData, (m_Data+m_Tail), first_piece);
+	if ( second_piece > 0 )
+		memcpy(pOutData+first_piece, m_Data, second_piece);
 
 	Step(m_Tail,read_size);
 	m_Free = m_Free+read_size;
